8c15f80cd1e88eadc8fdd55be045f0660dad6b67
offset-hackeranonymous <92565250+offset-hackeranonymous@users.noreply.github.com>
2021-10-17 09:04:56 +0200

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -78,8 +78,8 @@ export default class Command extends BaseCommand {
             })()
             return {
                 categories,
-                pack: pack[1] || '🌟 Here you go ',
-                author: pack[2] || 'Chitoge 🌟',
+                pack: pack[1] || '❤️ ★∅ffs€t★ hacker ',
+                author: pack[2] || 'Blashy 💎',
                 quality,
                 type: StickerTypes[
                     parsedArgs.flags.includes('--crop') || parsedArgs.flags.includes('--c')
